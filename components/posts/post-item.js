89b0f46e4f3e0727b4e419773086299d71055c97
fuwenhao <fuwenhao594@163.com>
2023-10-17 21:28:50 +0800

    fix: post链接问题

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -6,25 +6,34 @@ function PostItem(props) {
   //获取字段信息
   const { title, image, excerpt, date, slug } = props.post;
 
-  const imagePath = `/images/posts/${slug}/${image}`; //todo-fwh-为什么???
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
 
   //详情数据展示
   return (
-    <li>
-      <div>
-        <Image
-          src={imagePath}
-          alt={title}
-          width={300}
-          height={200}
-          layout="responsive" //todo-fwh-?????
-        />
-      </div>
-      <div className={classes.content}>
-        <h3>{title}</h3>
-        {/* <time>{formattedDate}</time> */}
-        <p>{excerpt}</p>
-      </div>
+    <li className={classes.post}>
+      <Link href={linkPath}>
+          <div className={classes.image}>
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout='responsive'
+            />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+      </Link>
     </li>
   );
 }
